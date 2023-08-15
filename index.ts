bf8511abd3c831fc942ff388900d8530bddf39f7
Jhonny <aranajhonny@users.noreply.github.com>
2023-08-15 14:59:53 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,60 @@
+import { nodes, root, state } from "membrane";
+import { Feed } from "feed";
+
+// Initialize an empty object to store feeds
+state.feeds = state.feeds ?? {};
+
+export async function configure({ args: { feed, feedOptions } }) {
+  state.feeds[feed] = new Feed(feedOptions);
+}
+
+export async function addItem({ args: { feed, item } }) {
+  if (state.feeds[feed]) {
+    // Workaround for membrane proxy bug
+    item = JSON.parse(JSON.stringify(item));
+    item.date = item.date ? new Date(item.date) : new Date();
+    state.feeds[feed].addItem(item);
+  }
+}
+
+export async function addCategory({ args: { feed, category } }) {
+  if (state.feeds[feed]) {
+    state.feeds[feed].addCategory(category);
+  }
+}
+
+export async function addContributor({ args: { feed, contributor } }) {
+  if (state.feeds[feed]) {
+    state.feeds[feed].addContributor(contributor);
+  }
+}
+
+export async function endpoint({ args }) {
+  const feed: string = args.path.slice(1);
+
+  switch (args.path) {
+    case "/":
+      return JSON.stringify({
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Object.keys(state.feeds)),
+      });
+    default:
+      if (state.feeds[feed]) {
+        return JSON.stringify({
+          status: 200,
+          headers: {
+            "Content-Type": "application/rss+xml",
+          },
+          body: state.feeds[feed].rss2(),
+        });
+      } else {
+        return JSON.stringify({
+          status: 404,
+          body: "Not Found",
+        });
+      }
+  }
+}
